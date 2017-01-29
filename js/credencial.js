22b43f3f64b94eb9759820e17b2093d7144d693f
mauricio hernández gómez <mauricio hernndez gmez>
2017-01-28 21:39:19 -0600

    Ultimo comit

---
+++ js/credencial.js
@@ -0,0 +1,12 @@
+var num1;
+var num2;
+var num3;
+
+num1 = prompt("Dame tu Primer nombre","Nombre");
+num2 = prompt("Dame tu Apellidos","Apellidos");
+num3 = prompt("Dame tu Alias","Alias");
+
+
+$("#nom").html("<b><br>Nombre: "+num1+"</b>")
+$("#ape").html("<b><br>Apellidos: "+num2+"</b>")
+$("#ali").html("<b><br>Alias: "+num3+"</b>")
